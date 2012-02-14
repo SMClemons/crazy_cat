e2acafb977cd08ee43fd88bc27bf4cd58a131d09
Steve Clemons <clemons@matcmadison.edu>
2012-02-13 19:01:34 -0600

    Crazy Cat commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrazyCat::Application.config.secret_token = 'f4c78200c899a9156c757cce4fbf6ccc1557a54d8e5b71e23aa4737cb4c80a6d2266568d6bcdfafd4bf69286c2c69cecf2110d98d896c759bbaefaaa1ef0f3a7'
